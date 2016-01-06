79ae1dfc96e44b995d61f3a0875cb3f0566f020b
横田直也 <naoya.yokota@recochoku.co.jp>
2016-01-06 12:47:27 +0900

    edit

---
+++ src/app/components/footer/footer.directive.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('sound_of_network')
+    .directive('acmeFooter', acmeFooter);
+
+  /** @ngInject */
+  function acmeFooter() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/footer/footer.html',
+      scope: {
+          creationDate: '='
+      },
+      controller: FooterController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function FooterController() {
+    }
+  }
+
+})();
