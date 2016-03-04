e90e7634677f54d4624748259a8ad64dd583d8b0
横田直也 <naoya.yokota@recochoku.co.jp>
2016-03-04 18:14:03 +0900

    edit

--- src/app/mypage/mypage.controller.js
+++ src/app/mypage/mypage.controller.js
@@ -6,15 +6,51 @@
     .controller('MyPageController', MyPageController);
 
   /** @ngInject */
-  function MyPageController() {
+  function MyPageController($scope, $location, $http, apihost) {
     var vm = this;
 
-    vm.awesomeThings = [];
+    vm.projects = [];
+    vm.getRequests = [];
+    vm.sendRequests = [];
+
+    var token = localStorage.getItem('id_token');
 
     activate();
 
     function activate() {
+
+      getMyProject();
+
+      getMyRequests();
+
+      getMySendRequests();
     }
 
+    function getMyProject() {
+      var api = 'http://' + apihost + '/api/me/projects?token=' + token;
+
+    }
+
+    function getMyRequests() {
+      var api = 'http://' + apihost + '/api/me/request/get?token=' + token;
+
+    }
+
+    function getMySendRequests() {
+      var api = 'http://' + apihost + '/api/me/request/send?token=' + token;
+
+    }
+
+    function requestApi(api, sC, eC) {
+      $http.get(api,
+          {headers: {
+                      'Content-Type' : 'application/json; charset=UTF-8'
+                    }})
+      .success(function(data) {
+        console.log(data);
+      })
+      .error(function(data, status) {
+      });
+    }
   }
 })();
