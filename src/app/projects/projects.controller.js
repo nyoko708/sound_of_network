46d2eac2032732f859002c6c2af7011b9e14ef5d
横田直也 <naoya.yokota@recochoku.co.jp>
2016-03-04 17:55:10 +0900

    edit

--- src/app/projects/projects.controller.js
+++ src/app/projects/projects.controller.js
@@ -9,6 +9,8 @@
   function ProjectsController($scope, $location, $http, apihost) {
     var vm = this;
 
+    vm.projects = [];
+
     /**
      * 遷移
      */
@@ -16,13 +18,28 @@
       $location.path( path );
     };
 
+    var token = localStorage.getItem('id_token');
+
+    // 自分が参加しているプロジェクト取得
+    var api = 'http://' + apihost + '/api/me/projects?token=' + token;
+    $http.get(api,
+    {headers: {
+                    'Content-Type' : 'application/json; charset=UTF-8'
+                  }})
+    .success(function(data) {
+      console.log(data);
+      vm.projects = data.projects;
+    })
+    .error(function(data, status) {
+      console.log("error. get my project.");
+    });
+
     /**
      * プロジェクトの作成
      */
     $scope.createProject = function(projectData) {
       var api = 'http://'+ apihost +'/api/project/create';
 
-      var token = localStorage.getItem('id_token');
       api = api + '?token=' + token;
 
       var access = 0;
