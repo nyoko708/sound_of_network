4a11621918961943827b92626f1382d883d95c78
横田直也 <naoya.yokota@recochoku.co.jp>
2016-03-24 16:24:16 +0900

    edit

--- src/app/components/apiRequest/apiRequest.service.js
+++ src/app/components/apiRequest/apiRequest.service.js
@@ -6,8 +6,39 @@
     .factory('apiRequest', apiRequest);
 
   /** @ngInject */
-  function apiRequest() {
+  function apiRequest($http) {
 
-    return "";
+    var headers = {headers: {'Content-Type' : 'application/json; charset=UTF-8'}};
+
+    /**
+     * GET リクエスト
+     */
+    var requestGet = function(api, sC, eC) {
+      $http.get(api, headers
+      .success(function(data) {
+        sC(data);
+      })
+      .error(function(data, status) {
+        eC(data, status);
+      });
+    };
+
+    /**
+     * POST リクエスト
+     */
+    var requestPost = function(api, args, sC, eC) {
+      $http.post(api, args, headers
+      .success(function(data) {
+        sC(data);
+      })
+      .error(function(data, status) {
+        eC(data, status);
+      });
+    };
+
+    return {
+      requestGet: requestGet,
+      requestPost: requestPost
+    };
   }
 })();
